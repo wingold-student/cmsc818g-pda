487507610ed1a1ccbe200ff071e9a53be08b96d3
yoonieshon <ykshon@umd.edu>
2022-04-15 17:33:12 -0400

    yoon

--- src/main/java/com/cmsc818g/StressContextEngine/StressContextEngine.java
+++ src/main/java/com/cmsc818g/StressContextEngine/StressContextEngine.java
@@ -1,21 +1,90 @@
 package com.cmsc818g.StressContextEngine;
+import java.time.Duration;
 
+import com.cmsc818g.StressManagementController;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 
 public class StressContextEngine extends AbstractBehavior<StressContextEngine.Command> {
 
     public interface Command {}
-
-    @Override
-    public Receive<Command> createReceive() {
-        // TODO Auto-generated method stub
-        return null;
+    private static final class engineResponse implements Command {
+      public final String message;
+  
+      public engineResponse(String message) {
+        this.message = message;
+      }
+    }//end of engineResponse
+  
+    public static Behavior<Command> create(ActorRef<StressManagementController.Command> controller) {
+      return Behaviors.setup(context -> new StressContextEngine(context, controller));
     }
-    
+
     public StressContextEngine(ActorContext<Command> context) {
         super(context);
-        //TODO Auto-generated constructor stub
+        System.out.println("StressContextEngine actor created");
+    }
+
+    private StressContextEngine(ActorContext<Command> context, ActorRef<StressManagementController.Command> controller) {
+      super(context);
+      System.out.println("StressContextEngine actor created");
+  
+      // asking someone requires a timeout, if the timeout hits without response
+      // the ask is failed with a TimeoutException
+        final Duration timeout = Duration.ofSeconds(3);
+  
+      context.ask(
+        StressManagementController.controllerResponse.class,
+        controller,
+          timeout,
+          // construct the outgoing message
+          (ActorRef<StressManagementController.controllerResponse> ref) -> new StressManagementController.controllerCommunication(ref),
+          // adapt the response (or failure to respond)
+          (response, throwable) -> {
+            if (response != null) {
+              return new engineResponse(response.message);
+            } else {
+              return new engineResponse("Request failed");
+            }
+          });
+  
+      // we can also tie in request context into an interaction, it is safe to look at
+      // actor internal state from the transformation function, but remember that it may have
+      // changed at the time the response arrives and the transformation is done, best is to
+      // use immutable state we have closed over like here.
+      final int requestId = 1;
+      context.ask(
+        StressManagementController.controllerResponse.class,
+          controller,
+          timeout,
+          // construct the outgoing message
+          (ActorRef<StressManagementController.controllerResponse> ref) -> new StressManagementController.controllerCommunication(ref),
+          // adapt the response (or failure to respond)
+          (response, throwable) -> {
+            if (response != null) {
+              return new engineResponse(requestId + ": " + response.message);
+            } else {
+              return new engineResponse(requestId + ": Request failed");
+            }
+          });
+    }
+  
+    @Override
+    public Receive<Command> createReceive() {
+      return newReceiveBuilder()
+          // the adapted message ends up being processed like any other
+          // message sent to the actor
+          .onMessage(engineResponse.class, this::onAdaptedResponse)
+          .build();
+    }
+  
+    private Behavior<Command> onAdaptedResponse(engineResponse response) {
+      getContext().getLog().info("Got response from StressManagementController: {}", response.message);
+      return this;
     }
 }
--- src/main/java/com/cmsc818g/StressManagementController.java
+++ src/main/java/com/cmsc818g/StressManagementController.java
@@ -1,22 +1,64 @@
 package com.cmsc818g;
+import com.cmsc818g.StressContextEngine.StressContextEngine;
 
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 
 public class StressManagementController extends AbstractBehavior<StressManagementController.Command> {
+  
+
+    public StressManagementController(ActorContext<Command> context) {
+        super(context);
+        System.out.println("controller actor created");
+    }
+
+    public static Behavior<StressManagementController.Command> create() {
+        return Behaviors.setup(StressManagementController::new);
+    }
 
     public interface Command {}
+    public static final class controllerCommunication implements Command {
+      public final ActorRef<controllerResponse> respondTo;
+  
+      public controllerCommunication(ActorRef<controllerResponse> respondTo) {
+        this.respondTo = respondTo;
+      }
+    }//end of class controllerCommunication
+
+    public static final class controllerResponse {
+      public final String message;
+  
+      public controllerResponse(String message) {
+        this.message = message;
+      }
+    }//end of class controllerResponse
 
     @Override
     public Receive<Command> createReceive() {
-        // TODO Auto-generated method stub
-        return null;
+        return newReceiveBuilder().onMessage(controllerCommunication.class, this::controllerCommunication).build();
     } 
 
-   public StressManagementController(ActorContext<Command> context) {
-        super(context);
-        //TODO Auto-generated constructor stub
+    private Behavior<Command> controllerCommunication(controllerCommunication message) {
+        message.respondTo.tell(new controllerResponse("I'm sorry. I'm afraid I can't do that."));
+        System.out.println(message);
+        return this;
     }
+    public static void main(String[] args)
+    {
+        ActorRef<StressManagementController.Command> pda = ActorSystem.create(StressManagementController.create(), "pda-system-controller");
+        System.out.println("pda-system started, controller working");
+        ActorSystem.create(StressContextEngine.create(pda), "pda-system-controller");
+
+        //ActorRef<StressManagementController.Command> pda = ActorSystem.create(StressManagementController.create(),"controller");
+        
+        //pda.tell(new StressManagementController.Command(" controller message whatever comes here"));
+
+        
+    }
+
 }
