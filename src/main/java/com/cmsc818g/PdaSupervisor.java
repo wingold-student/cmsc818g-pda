fcfbef51c1e35b1e86d88e5dc08e468d85199fa9
William Ingold <wingold@umd.edu>
2022-04-15 17:42:34 -0400

    Starts up the web server and assigns the routes

--- src/main/java/com/cmsc818g/PdaSupervisor.java
+++ src/main/java/com/cmsc818g/PdaSupervisor.java
@@ -1,6 +1,9 @@
 package com.cmsc818g;
 
-import akka.actor.typed.ActorSystem;
+import com.cmsc818g.StressUIManager.StressWebServer;
+import com.cmsc818g.StressUIManager.WebRoutes;
+
+import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.PostStop;
 import akka.actor.typed.javadsl.AbstractBehavior;
@@ -17,20 +20,20 @@ public class PdaSupervisor extends AbstractBehavior<Void> {
     private PdaSupervisor(ActorContext<Void> context) {
         super(context);
         context.getLog().info("PDA Application started");
+        ActorRef<StressWebServer.Command> webServer = context.spawn(StressWebServer.create(), "WebServer");
+        WebRoutes webRoutes = new WebRoutes(context.getSystem());
+        webServer.tell(new StressWebServer.StartServer(webRoutes.webRoutes()));
+        webServer.tell(new StressWebServer.Hello("Testing!"));
     }
 
     @Override
     public Receive<Void> createReceive() {
-        return newReceiveBuilder().onSignal(PostStop.class, signal -> onPostStop()).build();
+        return newReceiveBuilder()
+            .onSignal(PostStop.class, signal -> onPostStop()).build();
     }
 
     private PdaSupervisor onPostStop() {
         getContext().getLog().info("PDA Application stopped");
         return this;
-    }
-
-    public static void main( String[] args )
-    {
-        ActorSystem.create(PdaSupervisor.create(), "pda-system");
     }
 }
