04290577a61330587e9ea80c1e3ed6e399b51d25
William Ingold <wingold@umd.edu>
2022-04-16 13:08:12 -0400

    Starts and stops the server upon command

--- src/main/java/com/cmsc818g/StressUIManager/StressWebServer.java
+++ src/main/java/com/cmsc818g/StressUIManager/StressWebServer.java
@@ -2,9 +2,12 @@ package com.cmsc818g.StressUIManager;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.CompletionStage;
+import java.time.Duration;
 
+import akka.actor.typed.ActorRef;
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
+import akka.actor.typed.PostStop;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
@@ -20,6 +23,7 @@ import akka.http.javadsl.server.Route;
 public class StressWebServer extends AbstractBehavior<StressWebServer.Command> {
     public interface Command {}
 
+    /** Command to start the server with the supplied routes. */
     public static final class StartServer implements Command {
         final Route route;
 
@@ -28,12 +32,17 @@ public class StressWebServer extends AbstractBehavior<StressWebServer.Command> {
         }
     }
 
+    /** Just a sample message/command. */
     public static final class Hello implements Command {
         final String msg;
-
         public Hello(String msg) {
             this.msg = msg;
         }
+    }
+
+    /** Command to stop the server. */
+    public static final class StopServer implements Command {
+        public StopServer() {}
     }
 
     public static Behavior<Command> create() {
@@ -44,24 +53,35 @@ public class StressWebServer extends AbstractBehavior<StressWebServer.Command> {
     public Receive<Command> createReceive() {
         return newReceiveBuilder()
             .onMessage(StartServer.class, this::onStartServer)
+            .onMessage(StopServer.class, this::onStopServer)
             .onMessage(Hello.class, this::onHello)
+            .onSignal(PostStop.class, signal -> onPostStop())
             .build();
     } 
 
-   public StressWebServer(ActorContext<Command> context) {
+    private CompletionStage<ServerBinding> futureBinding;
+
+    public StressWebServer(ActorContext<Command> context) {
         super(context);
         context.getLog().info("Web Server Actor started");
     }
 
+    /**
+     * Start up the server with the provided routes (within the msg).
+     * @param msg Holds the routes to serve
+     * @return this
+     */
     private Behavior<Command> onStartServer(StartServer msg) {
         getContext().getLog().info("Starting web server...");
         ActorSystem<?> system = getContext().getSystem();
         Route route = msg.route;
 
-        CompletionStage<ServerBinding> futureBinding =
+        // Waits for the binding to succeed essentially
+        this.futureBinding =
             Http.get(getContext().getSystem()).newServerAt("localhost", 8080).bind(route);
 
-        futureBinding.whenComplete((binding, exception) -> {
+        // Once succeeded, print out the data and that it is started
+        this.futureBinding.whenComplete((binding, exception) -> {
             if (binding != null) {
                 InetSocketAddress address = binding.localAddress();
                 system.log().info("Server online at http://{}:{}",
@@ -73,6 +93,34 @@ public class StressWebServer extends AbstractBehavior<StressWebServer.Command> {
             }
         });
 
+        return this;
+    }
+
+    /**
+     * Attempts to 'gracefully' shutdown the server if it is running.
+     * 
+     * TODO: Return boolean on if it was running or successful?
+     */
+    private void shutdownServer() {
+        this.futureBinding.whenComplete((binding, exception) -> {
+            if (binding != null) {
+                getContext().getLog().info("Stopping the web server...");
+                binding.terminate(Duration.ofSeconds(3));
+            } else {
+                getContext().getLog().warn("Server not running or bound");
+            }
+        });
+    }
+
+    private StressWebServer onPostStop() {
+        getContext().getLog().info("StressWebServer stopped");
+        shutdownServer();
+        return this;
+    }
+
+    private Behavior<Command> onStopServer(StopServer msg) {
+        getContext().getLog().info("Requested to stop server");
+        shutdownServer();
         return this;
     }
 
