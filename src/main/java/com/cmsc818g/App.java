e3f97dc0dc71ae72f879c76a86664ffc0920c12d
William Ingold <wingold@umd.edu>
2022-04-16 12:23:47 -0400

    Minor docs

--- src/main/java/com/cmsc818g/App.java
+++ src/main/java/com/cmsc818g/App.java
@@ -3,6 +3,14 @@ package com.cmsc818g;
 import java.io.IOException;
 
 import akka.actor.typed.ActorSystem;
+
+/**
+ * Just something to start up the system and keep it running.
+ * It will keep running until the user inputs something into the terminal.
+ * 
+ * We could potentially have a terminal interface along with the web interface,
+ * maybe just for testing purposes?
+ */
 public class App 
 {
     public static void main( String[] args )
