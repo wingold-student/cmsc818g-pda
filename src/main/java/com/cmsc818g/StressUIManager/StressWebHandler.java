5101e60feda96f8c3809c041e37c91b6682ffeb1
William Ingold <wingold@umd.edu>
2022-04-16 13:04:26 -0400

    Docs

--- src/main/java/com/cmsc818g/StressUIManager/StressWebHandler.java
+++ src/main/java/com/cmsc818g/StressUIManager/StressWebHandler.java
@@ -10,10 +10,18 @@ import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 
+/**
+ * StressWebHandler will be the middle-man for asking around for desired data.
+ * 
+ * This is the actor the WebRoutes will use when the user requests data from the
+ * PDA. It will know the necessary actors(systems, managers, etc.) to query for
+ * desired data. It can also serve it in a JSON format.
+ */
 public class StressWebHandler extends AbstractBehavior<StressWebHandler.Command> {
 
     public interface Command {}
 
+    /** Currently just a test request message for some JSON data. */
     public final static class GetTestJSON implements Command {
         public final ActorRef<GetTestJSONResponse> replyTo;
         public GetTestJSON(ActorRef<GetTestJSONResponse> replyTo) {
@@ -21,6 +29,9 @@ public class StressWebHandler extends AbstractBehavior<StressWebHandler.Command>
         }
     }
 
+    /** Hands back the data. Note it will be part of the 'testData' field
+     * in the json.
+     */
     public final static class GetTestJSONResponse {
         public final TestData testData;
         public GetTestJSONResponse(TestData testData) {
@@ -37,6 +48,7 @@ public class StressWebHandler extends AbstractBehavior<StressWebHandler.Command>
         super(context);
     }
 
+    /** TestData is just an example class for holding JSON data. */
     public final static class TestData {
         public final String field;
 
