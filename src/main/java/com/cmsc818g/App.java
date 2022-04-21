a748afe7c40470dc3612d66a05b68b3b3c686dae
William Ingold <wingold@umd.edu>
2022-04-21 12:24:15 -0400

    SQL testing with Yoon

--- src/main/java/com/cmsc818g/App.java
+++ src/main/java/com/cmsc818g/App.java
@@ -1,5 +1,8 @@
 package com.cmsc818g;
-
+import java.sql.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import akka.actor.typed.ActorSystem;
 /**
@@ -13,6 +16,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        /*
         final ActorSystem<StressManagementController.Command> system = ActorSystem.create(StressManagementController.create(), "pda-system");
         //system.tell(new StressManagementController.controllerProcess("start")) ;
             
@@ -22,7 +26,39 @@ public class App
             e.printStackTrace();
         }
         system.terminate();
+*/
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet set = null;
+        try{  
+            Class.forName("org.sqlite.JDBC");  
+            connection = DriverManager.getConnection("jdbc:sqlite:Demo.db");  
+            //connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/HealthData","root","");  
+            statement = connection.createStatement();   
+           // statement.executeUpdate("CREATE DATABASE STUDENTS");
+            //set = statement.executeQuery("select * from Scenario");  
+           // while(set.next()){
+            //    System.out.println("There is result set");
+           // }
+            //FileReader fr = new FileReader(new File("Scenario.sql"));
+           // connection.close();  
+            }catch(SQLException se){
+                System.out.println("Connection Failed!");
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch blocks
+                e.printStackTrace();
+            }finally{
+                try {
+                if(connection != null)
+                    connection.close();
+                System.out.println("Connection closed !!");
+                } catch (SQLException e) {
+                e.printStackTrace();
+                }
+            }
+        }
    
-    }
+    
   
 }
