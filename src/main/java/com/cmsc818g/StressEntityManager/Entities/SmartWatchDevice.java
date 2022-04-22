0f4db1c89bf6a0adb03c843b13e7342d8d31508f
William Ingold <wingold@umd.edu>
2022-04-22 13:29:34 -0400

    Update messages. Reads a row of data from db

--- src/main/java/com/cmsc818g/StressEntityManager/Entities/SmartWatchDevice.java
+++ src/main/java/com/cmsc818g/StressEntityManager/Entities/SmartWatchDevice.java
@@ -1,5 +1,6 @@
 package com.cmsc818g.StressEntityManager.Entities;
 
+import akka.Done;
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.PostStop;
@@ -7,92 +8,172 @@ import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
+import akka.http.javadsl.model.DateTime;
+import akka.japi.Option;
+import akka.pattern.StatusReply;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public class SmartWatchDevice extends AbstractBehavior<SmartWatchDevice.Command> {
+    /************************************* 
+     * MESSAGES IT RECEIVES 
+     *************************************/
     public interface Command {}
+    public static final class ReadRowOfData implements Command {
+        final int rowNumber;
+        final ActorRef<StatusReply<Done>> replyTo;
+
+        public ReadRowOfData(int rowNumber,  ActorRef<StatusReply<Done>> replyTo) {
+            this.rowNumber = rowNumber;
+            this.replyTo = replyTo;
+        }
+    }
 
     public static final class ReadHeartrate implements Command {
-        final long requestId;
-        final ActorRef<RespondHeartrate> replyTo;
+        final ActorRef<HeartRateReading> replyTo;
 
-        public ReadHeartrate(long requestId, ActorRef<RespondHeartrate> replyTo) {
-            this.requestId = requestId;
+        public ReadHeartrate(ActorRef<HeartRateReading> replyTo) {
             this.replyTo = replyTo;
         }
     }
 
-    public static final class RespondHeartrate {
-        final long requestId;
+    public static final class ReadBloodPressure implements Command {
+        final ActorRef<BloodPressureReading> replyTo;
+
+        public ReadBloodPressure(ActorRef<BloodPressureReading> replyTo) {
+            this.replyTo = replyTo;
+        }
+    }
+
+    /************************************* 
+     * MESSAGES IT SENDS
+     *************************************/
+    public interface Response {}
+
+    public static final class HeartRateReading implements Response {
         final Optional<Integer> value;
+        final Optional<DateTime> readingTime;
 
-        public RespondHeartrate(long requestId, Optional<Integer> value) {
-            this.requestId = requestId;
+        public HeartRateReading(Optional<Integer> value, Optional<DateTime> readingTime) {
             this.value = value;
+            this.readingTime = readingTime;
         }
     }
 
-    public static final class RecordHeartrate implements Command {
-        final long requestId;
-        final int value;
-        final ActorRef<HeartrateRecorded> replyTo;
+    public static final class BloodPressureReading implements Response {
+        final Optional<String> value;
+        final Optional<DateTime> readingTime;
 
-        public RecordHeartrate(long requestId, int value, ActorRef<HeartrateRecorded> replyTo) {
-            this.requestId = requestId;
+        public BloodPressureReading(Optional<String> value, Optional<DateTime> readingTime) {
             this.value = value;
-            this.replyTo = replyTo;
+            this.readingTime = readingTime;
         }
     }
 
-    public static final class HeartrateRecorded {
-        final long requestId;
+    /************************************* 
+     * CREATION 
+     *************************************/
 
-        public HeartrateRecorded(long requestId) {
-            this.requestId = requestId;
-        }
+    public static Behavior<Command> create(String databaseURI, String tableName) {
+        return Behaviors.setup(context -> new SmartWatchDevice(context, databaseURI, tableName));
     }
 
-    public static Behavior<Command> create(String groupId, String deviceId) {
-        return Behaviors.setup(context -> new SmartWatchDevice(context, groupId, deviceId));
-    }
+    private final String databaseURI;
+    private final String tableName;
+    private Optional<Integer> lastHeartbeat;
+    private Optional<String> lastBloodPressure;
+    private Optional<DateTime> lastReadingTime;
 
-    private final String groupId;
-    private final String deviceId;
-
-    private Optional<Integer> lastHeartrateReading = Optional.empty();
-
-    private SmartWatchDevice(ActorContext<Command> context, String groupId, String deviceId) {
+    private SmartWatchDevice(ActorContext<Command> context, String databaseURI, String tableName) {
         super(context);
-        this.groupId = groupId;
-        this.deviceId = deviceId;
+        this.databaseURI = databaseURI;
+        this.tableName = tableName;
+        this.lastHeartbeat = Optional.empty();
+        this.lastBloodPressure = Optional.empty();
+        this.lastReadingTime = Optional.empty();
 
-        context.getLog().info("Smartwatch actor {}-{} started", groupId, deviceId);
+        context.getLog().info("Smartwatch actor starting");
     }
+
+    /************************************* 
+     * MESSAGE HANDLING 
+     *************************************/
 
     @Override
     public Receive<Command> createReceive() {
         return newReceiveBuilder()
+            .onMessage(ReadRowOfData.class, this::onReadRowOfData)
             .onMessage(ReadHeartrate.class, this::onReadHeartrate)
-            .onMessage(RecordHeartrate.class, this::onRecordHeartrate)
+            .onMessage(ReadBloodPressure.class, this::onReadBloodPressure)
             .onSignal(PostStop.class, signal -> onPostStop())
             .build();
     }
 
-    private Behavior<Command> onReadHeartrate(ReadHeartrate r) {
-        r.replyTo.tell(new RespondHeartrate(r.requestId, lastHeartrateReading));
+    private Connection connectToDB() {
+        Connection conn = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection(this.databaseURI);
+        } catch (SQLException e) {
+            // entityManager.tell(I've failed!)
+            getContext().getLog().error("Failed to connect to the database {} with error {}", databaseURI, e);
+            getContext().stop(getContext().getSelf());
+        } catch (ClassNotFoundException e) {
+            getContext().getLog().error("Failed to load JDBC driver", e);
+            getContext().stop(getContext().getSelf());
+        }
+
+        return conn;
+    }
+
+    private Behavior<Command> onReadRowOfData(ReadRowOfData msg) {
+        Connection conn = this.connectToDB();
+        ResultSet results = null;
+
+        String query = "SELECT id, DateTime, Heartbeat, Bloodpressure FROM " + this.tableName + " WHERE id = ?";
+        try {
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setInt(1, msg.rowNumber);
+
+            results = statement.executeQuery();
+            results.next();
+
+            this.lastHeartbeat = Optional.of(results.getInt("Heartbeat"));
+            this.lastBloodPressure = Optional.of(results.getString("Bloodpressure"));
+
+            String dateTimeStr = results.getString("DateTime");
+            this.lastReadingTime = DateTime.fromIsoDateTimeString(dateTimeStr);
+
+            results.close();
+            msg.replyTo.tell(StatusReply.Ack());
+
+        } catch (SQLException e) {
+            getContext().getLog().error("Failed to query the with error {}", e);
+            msg.replyTo.tell(StatusReply.error("Failed query"));
+            getContext().stop(getContext().getSelf());
+        } 
+
         return this;
     }
 
-    private Behavior<Command> onRecordHeartrate(RecordHeartrate r) {
-        getContext().getLog().info("Recorded heartrate reading {} with {}", r.value, r.requestId);
-        lastHeartrateReading = Optional.of(r.value);
-        r.replyTo.tell(new HeartrateRecorded(r.requestId));
+    private Behavior<Command> onReadHeartrate(ReadHeartrate msg) {
+        msg.replyTo.tell(new HeartRateReading(this.lastHeartbeat, this.lastReadingTime));
+        return this;
+    }
+
+    private Behavior<Command> onReadBloodPressure(ReadBloodPressure msg) {
+        msg.replyTo.tell(new BloodPressureReading(this.lastBloodPressure, this.lastReadingTime));
         return this;
     }
 
     private SmartWatchDevice onPostStop() {
-        getContext().getLog().info("Smartwatch actor {}-{} stopped", groupId, deviceId);
+        getContext().getLog().info("Smartwatch actor stopped");
         return this;
     }
 }
