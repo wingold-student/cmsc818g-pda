05055e1c452a9b737c4e6e4d31fe79e94e5052b5
William Ingold <wingold@umd.edu>
2022-04-22 13:40:17 -0400

    Update spawning of three entities

--- src/main/java/com/cmsc818g/StressEntityManager/StressEntityManager.java
+++ src/main/java/com/cmsc818g/StressEntityManager/StressEntityManager.java
@@ -3,6 +3,10 @@ package com.cmsc818g.StressEntityManager;
 import java.util.ArrayList;
 
 import com.cmsc818g.StressManagementController;
+import com.cmsc818g.StressEntityManager.Entities.CalendarCommand;
+import com.cmsc818g.StressEntityManager.Entities.CalendarEntity;
+import com.cmsc818g.StressEntityManager.Entities.PhoneEntity;
+import com.cmsc818g.StressEntityManager.Entities.SmartWatchDevice;
 
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
@@ -12,7 +16,9 @@ import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 
 public class StressEntityManager extends AbstractBehavior<StressEntityManager.Command> {
-    
+    /************************************* 
+     * MESSAGES IT RECEIVES 
+     *************************************/
     public interface Command {}
     public static class entityManagerGreet implements Command {
         public final ActorRef<StressManagementController.Command> replyTo;
@@ -20,17 +26,47 @@ public class StressEntityManager extends AbstractBehavior<StressEntityManager.Co
         public entityManagerGreet(ActorRef<StressManagementController.Command> replyTo) {
           this.replyTo = replyTo;
         }
-      }//end of class entityManagerGreet
+    }//end of class entityManagerGreet
 
-    public static Behavior<Command> create() {
-      return Behaviors.setup(context -> new StressEntityManager(context));
+    /************************************* 
+     * MESSAGES IT SENDS
+     *************************************/
+
+    
+    /************************************* 
+     * CREATION 
+     *************************************/
+    private ActorRef<PhoneEntity.Command> phoneEntity;
+    private ActorRef<CalendarCommand> calendarEntity;
+    private ActorRef<SmartWatchDevice.Command> smartWatchEntity;
+
+    private final String phoneName = "PhoneEntity";
+    private final String calendarName = "CalendarEntity";
+    private final String smartWatchName = "SmartWatchEntity";
+    private ArrayList<String> entities;
+
+    public static Behavior<Command> create(String databaseURI, String tableName) {
+      return Behaviors.setup(context -> new StressEntityManager(context, databaseURI, tableName));
     }
 
-    public StressEntityManager(ActorContext<Command> context) {
+    public StressEntityManager(ActorContext<Command> context, String databaseURI, String tableName) {
         super(context);
         getContext().getLog().info("Entity Manager actor created");
-        //TODO: spawn entities
+
+        phoneEntity = context.spawn(PhoneEntity.create(context.getSelf(), databaseURI, tableName), phoneName);
+        calendarEntity = context.spawn(CalendarEntity.create(databaseURI, tableName), calendarName);
+        smartWatchEntity = context.spawn(SmartWatchDevice.create(databaseURI, tableName), smartWatchName);
+
+        entities = new ArrayList<String>();
+
+        entities.add(smartWatchName);
+        entities.add(phoneName);
+        entities.add(calendarName);
     }
+
+    /************************************* 
+     * MESSAGE HANDLING 
+     *************************************/
   
     @Override
     public Receive<Command> createReceive() {
@@ -39,16 +75,9 @@ public class StressEntityManager extends AbstractBehavior<StressEntityManager.Co
     }
   
     private Behavior<Command> onEngineResponse(entityManagerGreet message) { //when receive message
-        //get information of connected entities
-        getContext().getLog().info("Get entity lists and send msg back");
-        ArrayList<String> entities = new ArrayList<String>();
-        entities.add("SmartWatch");
-        entities.add("BloodPressureCuff");
-        entities.add("Phone");
-        entities.add("WorkCalender");
-        entities.add("SchoolCalender");
-
-        message.replyTo.tell(new StressManagementController.EntityManagerToController("entityList", entities)); 
+      //get information of connected entities
+      getContext().getLog().info("Get entity lists and send msg back");
+      message.replyTo.tell(new StressManagementController.EntityManagerToController("entityList", entities)); 
       return this;
     }
 
