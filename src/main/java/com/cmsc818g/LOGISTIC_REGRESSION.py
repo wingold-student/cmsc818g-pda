a7c9d90c3c95be2f91a02e1bf943ba3774e79f76
Irtaza Shahid <70219647+IrtazaShahidUMD@users.noreply.github.com>
2022-05-10 00:39:44 -0400

    Updated with model saving and loading

--- src/main/java/com/cmsc818g/LOGISTIC_REGRESSION.py
+++ src/main/java/com/cmsc818g/LOGISTIC_REGRESSION.py
@@ -17,6 +17,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, mean_squared_error 
 import glob as glob
+import pickle
 
 #id,sleep-hours,busyness,bp-systolic,bp-diastolic,heart-rate,target
 #1,3,2,132,80,79,3
@@ -43,34 +44,38 @@ class LOGISTIC_REGRESSION:
     
     dataset = pd.read_csv('../../../../../src/main/resources/knn_data.csv', index_col="id")
     datatop = dataset.head()
+
+    model_filename = '../../../../../src/main/resources/LogisticRegression_model.sav' # filename for the model
     # print(datatop)
   
     X = dataset.iloc[:, [1,2,3,4,5]].values # splits the data and make separate array X to hold attributes.
     y = dataset.iloc[:, -1].values  # splits the data and makes a separate array y to hold corresponding labels.
-    # print(X.shape)
-    # print(y.shape)
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.03, random_state=0) # training 80%, testing 20%
     k_split = 200   
     X_train = X[:k_split]
     y_train = y[:k_split]
     X_test = X[k_split:]
     y_test = y[k_split:]
-    # print(y[k_split:])
+
+    ##### Logistic Regression #####
+    if(running_with_java == 0):
+        reg = LogisticRegression(penalty='l2',random_state=None, solver='lbfgs', max_iter=10000, multi_class='auto').fit(X, y)
+        #print("X_train: ", X_train.shape)
+        #print("Y_train: ", y_train.shape)
+    
+        pickle.dump(reg, open(model_filename, 'wb'))
+        
+        y_pred = reg.predict(X_test)
+        
+        score = np.sum((y_pred - y_test)==0)/len(y_pred)
+        print("accuracy score: ", score)
+    
+    
     if(running_with_java):
         X_test = health_data[0:]
         y_test = health_data[0:]
 
-    ##### Logistic Regression #####
-    reg = LogisticRegression(penalty='l2',random_state=None, solver='lbfgs', max_iter=10000, multi_class='auto').fit(X, y)
-    #print("X_train: ", X_train.shape)
-    #print("Y_train: ", y_train.shape)
-    
-    y_pred = reg.predict(X_test)
-    if(running_with_java == 0):
-        score = np.sum((y_pred - y_test)==0)/len(y_pred)
-        print("accuracy score: ", score)
-    
-    if(running_with_java):
+        reg = pickle.load(open(model_filename, 'rb'))
+        y_pred = reg.predict(X_test)
         print(y_pred)    
 
     
